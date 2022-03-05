4ecef3ac64824ec227d2114bf99cb82816cb6d9c
Alvax10 <alvaro695547@gmail.com>
2022-03-05 14:53:03 -0300

    fixing geoloc

--- src/components/GiveLocation/giveLocation.tsx
+++ src/components/GiveLocation/giveLocation.tsx
@@ -19,7 +19,7 @@ export function ExtractLocation() {
         if (userData && !email) {
             setEmail(userData["email"]);
         }
-        if (geoloc) {
+        if (data["_geoloc"]) {
             // console.log("Ya hay loc: ", data["_geoloc"]);
             setGeoloc({ lat: data["_geoloc"]["lat"], lng: data["_geoloc"]["lng"] });
             navigate("/home");
